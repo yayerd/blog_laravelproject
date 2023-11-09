22fe0884d9d80a09a1102ea4d1f919f9ace0572d
Yaye Rokhaya <ladyrokhaya@gmail.com>
2023-11-09 11:27:44 +0000

    2me commit: Creation du dossier catégorie ses vues et routes

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,11 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/listecategorie', function () {
+    return view('categories.liste_categorie');
+});
+
+Route::get('/categories/ajouter', function () {
+    return view('categories.ajouter_categorie');
+});
